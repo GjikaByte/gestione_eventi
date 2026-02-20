db402af9479c44019c6b13bfb602cbc5289537d6
Andi Gjika <andi.gjika@outlook.com>
2026-02-20 11:14:27 +0100

    Improved error handling

---
+++ src/main/java/andi/gestione_eventi/DTOs/ErrorsPayload.java
@@ -0,0 +1,13 @@
+package andi.gestione_eventi.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ErrorsPayload {
+    private String message;
+    private LocalDateTime timestamp;
+}
---
+++ src/main/java/andi/gestione_eventi/exceptions/ErrorsHandler.java
@@ -0,0 +1,35 @@
+package andi.gestione_eventi.exceptions;
+
+import andi.gestione_eventi.DTOs.ErrorsPayload;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+
+public class ErrorsHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // 400
+    public ErrorsPayload handleBadRequest(BadRequestException ex) {
+        return new ErrorsPayload(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND) // 404
+    public ErrorsPayload handleNotFound(NotFoundException ex) {
+        return new ErrorsPayload(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 500
+    public ErrorsPayload handleGenericServerError(Exception ex) {
+        ex.printStackTrace();
+        return new ErrorsPayload("C'è stato un errore lato server", LocalDateTime.now());
+
+    }
+
+}
